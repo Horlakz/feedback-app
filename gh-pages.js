e57a6f97b048ced3e8f26699fb604c20d6ff7743
Horlakz <horlakz@protonmail.com>
2022-03-14 18:54:10 +0100

    edited gh-page.js file

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,16 @@
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
 ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
-        user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
-        }
+  "public", // path to public directory
+  {
+    branch: "gh-pages",
+    repo: "https://github.com/horlakz/feedback-app.git", // Update to point to your repository
+    user: {
+      name: "Horlakz", // update to use your name
+      email: "officialhorlakz@gmail.com", // Update to use your email
     },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+  },
+  () => {
+    console.log("Deploy Complete!");
+  }
+);
