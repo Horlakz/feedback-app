d49742bb83ca8e0be0897de8b26abec3dfd8ab23
Horlakz <horlakz@protonmail.com>
2022-03-14 19:27:23 +0100

    fixed average rating showing NaN when it's zero

--- src/components/FeedBackStats.svelte
+++ src/components/FeedBackStats.svelte
@@ -1,23 +1,23 @@
-
 <script>
-    import {FeedbackStore} from '../stores'
+  import { FeedbackStore } from "../stores";
 
-    $: count = $FeedbackStore.length;
-    $: average = $FeedbackStore.reduce((acc, cur) => acc + cur.rating, 0) / count;
+  $: count = $FeedbackStore.length;
+  $: average = $FeedbackStore.reduce((acc, cur) => acc + cur.rating, 0) / count;
 </script>
 
 <div class="feedback-stats">
-    <h4><span style="color: red;">{count}</span> Reveiws</h4>
-    {#if average ===   NaN}
+  <h4><span style="color: red;">{count}</span> Reveiws</h4>
+  {#if count === 0}
     <h4><span style="color: red;">0</span> Average Rating</h4>
-    {/if}
+  {:else}
     <h4><span style="color: red;">{average}</span> Average Rating</h4>
+  {/if}
 </div>
 
 <style>
-    .feedback-stats {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+  .feedback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
