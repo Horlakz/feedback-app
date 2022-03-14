dd33466e195a135bfe3fdf4e8b3a847ad340976a
Horlakz <horlakz@protonmail.com>
2022-03-14 19:27:55 +0100

    reduced dummy data from one to three

--- src/stores.js
+++ src/stores.js
@@ -1,19 +1,9 @@
-import {writable} from 'svelte/store'
+import { writable } from "svelte/store";
 
-export const FeedbackStore  = writable([
-    {
-        id: 1,
-        rating: 10,
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum iure sunt et placeat molestias totam aliquid adipisci quisquam molestiae fugit?'
-    },
-    {
-        id: 2,
-        rating: 6,
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum iure sunt et placeat molestias totam aliquid adipisci quisquam molestiae fugit?'
-    },
-    {
-        id: 3,
-        rating: 8,
-        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum iure sunt et placeat molestias totam aliquid adipisci quisquam molestiae fugit?'
-    },
-])
+export const FeedbackStore = writable([
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum iure sunt et placeat molestias totam aliquid adipisci quisquam molestiae fugit?",
+  },
+]);
